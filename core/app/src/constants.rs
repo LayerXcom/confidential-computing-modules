81b596101574281492d47c0bbfc5738d9943b801
osuketh <dish230@gmail.com>
2019-11-03 22:07:06 +0900

    Implement ocall_get_quote

---
+++ core/app/src/constants.rs
@@ -0,0 +1,3 @@
+pub const ENCLAVE_TOKEN: &str = "../bin/enclave.token";
+pub const ENCLAVE_FILE: &str = "../bin/enclave.signed.so";
+pub const DEBUG: i32 = 1;
--- core/app/src/main.rs
+++ core/app/src/main.rs
@@ -1,6 +1,22 @@
+use self::init_enclave::init_enclave;
+
 mod init_enclave;
 mod ocalls;
+mod constants;
 
 fn main() {
+    let enclave = match init_enclave() {
+        Ok(r) => {
+            println!("[+] Init Enclave Successful {}!", r.geteid());
+            r
+        },
+        Err(x) => {
+            println!("[-] Init Enclave Failed {}!", x.as_str());
+            return;
+        },
+    };
 
+    println!("[+] Done!");
+
+    enclave.destroy();
 }
