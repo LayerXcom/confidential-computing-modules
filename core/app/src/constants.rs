7e436b2337612584bd515e0c6282f84608a62493
osuketh <dish230@gmail.com>
2019-11-07 12:10:42 +0900

    Add a test for getting report

--- core/app/src/constants.rs
+++ core/app/src/constants.rs
@@ -3,8 +3,8 @@ pub const ENCLAVE_FILE: &str = "../bin/enclave.signed.so";
 pub const DEBUG: i32 = 1;
 
 pub const SPID: &str = "2C149BFC94A61D306A96211AED155BE9";
-pub const IAS_URL: &str = "api.trustedservices.intel.com";
+pub const IAS_URL: &str = "https://api.trustedservices.intel.com/sgx/dev/attestation/v3/report";
 pub const SIGRL_SUFFIX : &str = "/sgx/dev/attestation/v3/sigrl/";
 pub const REPORT_SUFFIX	: &str = "/sgx/dev/attestation/v3/report";
 
-pub const ATTESTATION_SERVICE_DEFAULT_RETRIES: u32 = 10;
+pub const IAS_DEFAULT_RETRIES: u32 = 10;
