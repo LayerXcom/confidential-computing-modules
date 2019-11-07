cfffc745fa53041f51ef3a1f4c8e96a46d7785d4
osuketh <dish230@gmail.com>
2019-11-07 19:50:59 +0900

    Fix .gitignore

--- core/app/src/web3/erc20.rs
+++ core/app/src/web3/erc20.rs
@@ -1,4 +1,5 @@
 use std::sync::Arc;
+use crate::error::*;
 
 use web3::{
     Web3,
@@ -8,17 +9,25 @@ use web3::{
 };
 
 #[derive(Debug)]
-pub struct Erc20Contract {
+pub struct AnonymousErc20Contract {
     web3: Arc<Web3<Http>>,
     eloop: EventLoopHandle,
     contract: Contract<Http>,
     account: Address,
 }
 
-// pub trait Erc20Gets {
+// impl AnonymousErc20Contract {
+//     pub fn from_deployed<P: AsRef<path>>(
+//         contract_address: &str,
+//         abi_path: P,
 
+//     )
 // }
 
-// pub trait Erc20Posts {
+// pub trait Erc20Gets {
+//     fn get_balances(&self, offset: U256, len: U256) -> Result<Vec<String>>;
+// }
 
+// pub trait Erc20Posts<G> {
+//     fn tranfer(&self, update_balance: String, report: String, signature: String, gas: G, confirmations: usize) -> Result<TransactionReceipt>;
 // }
