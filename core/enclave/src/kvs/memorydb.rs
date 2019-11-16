e380a5fc53bd996e56fb30591e3e6178698525a2
osuketh <dish230@gmail.com>
2019-11-16 19:20:15 +0900

    Implement MemoryKVS

--- core/enclave/src/kvs/memorydb.rs
+++ core/enclave/src/kvs/memorydb.rs
@@ -4,6 +4,34 @@ use std::{
 };
 use anonify_common::types::*;
 use super::*;
+use crate::error::Result;
 
-pub struct MemoryKVS(RwLock<BTreeMap<Address, DBValue>>);
+pub struct MemoryKVS(RwLock<BTreeMap<Vec<u8>, DBValue>>);
 
+impl MemoryKVS {
+    pub fn new() -> Self {
+        MemoryKVS(RwLock::new(BTreeMap::new()))
+    }
+}
+
+impl traits::KVS for MemoryKVS {
+    fn get(&self, key: &[u8]) -> Option<DBValue> {
+        let d = self.0.read().unwrap();
+        d.get(key).cloned()
+    }
+
+    fn write(&self, tx: DBTx) {
+        let mut d = self.0.write().unwrap();
+        let ops = tx.ops;
+        for op in ops {
+            match op {
+                DBOp::Insert { key, value } => {
+                    d.insert(key.into_vec(), value);
+                },
+                DBOp::Delete { key } => {
+                    d.remove(&*key);
+                },
+            }
+        }
+    }
+}
--- core/enclave/src/kvs/traits.rs
+++ core/enclave/src/kvs/traits.rs
@@ -5,5 +5,7 @@ use crate::error::Result;
 pub trait KVS: Sync + Send {
     fn tx(&self) -> DBTx { DBTx::new() }
 
-    fn get(&self, key: &[u8]) -> Result<Option<DBValue>>;
+    fn get(&self, key: &[u8]) -> Option<DBValue>;
+
+    fn write(&self, tx: DBTx);
 }
