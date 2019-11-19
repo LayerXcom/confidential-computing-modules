a4981177d69c2707973ed71b95c224fd2264897d
osuketh <dish230@gmail.com>
2019-11-19 13:06:54 +0900

    Implement rng gen in enclave

--- core/enclave/src/error.rs
+++ core/enclave/src/error.rs
@@ -9,11 +9,18 @@ pub type Result<T> = std::result::Result<T, EnclaveError>;
 #[derive(Debug)]
 pub enum EnclaveError {
     IoError(io::Error),
+    SgxError{ err: sgx_types::sgx_status_t },
 }
 
 impl From<io::Error> for EnclaveError {
-    fn from(e: io::Error) -> Self {
-        EnclaveError::IoError(e)
+    fn from(err: io::Error) -> Self {
+        EnclaveError::IoError(err)
+    }
+}
+
+impl From<sgx_types::sgx_status_t> for EnclaveError {
+    fn from(err: sgx_types::sgx_status_t) -> Self {
+        EnclaveError::SgxError{ err }
     }
 }
 
@@ -21,6 +28,7 @@ impl fmt::Display for EnclaveError {
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
         match *self {
             EnclaveError::IoError(ref err) => write!(f, "I/O error: {}", err),
+            EnclaveError::SgxError{ err } => write!(f, "Sgx Error: {:?}", err),
         }
     }
 }
--- core/enclave/src/state.rs
+++ core/enclave/src/state.rs
@@ -1,6 +1,6 @@
 //! State transition functions for anonymous asset
 use anonify_common::types::*;
-use crate::crypto::{AES256GCM, SymmetricKey};
+use crate::crypto::*;
 use crate::kvs::DBValue;
 use std::prelude::v1::*;
 
@@ -8,12 +8,12 @@ pub trait AnonymousAssetSTF {
     fn transfer(from: PubKey, to: PubKey, amount: Value) -> State<NewRand>;
 }
 
-pub struct OldRand(Vec<u8>);
-pub struct NewRand(Vec<u8>);
+pub struct OldRand([u8; 32]);
+pub struct NewRand([u8; 32]);
 
 #[derive(Debug, Clone)]
 pub struct State<R> {
-    address: PubKey,
+    pubkey: PubKey,
     balance: Value,
     randomness: R,
 }
@@ -32,7 +32,13 @@ impl State<OldRand> {
 
 impl From<State<OldRand>> for State<NewRand> {
     fn from(s: State<OldRand>) -> Self {
-        unimplemented!();
+        let mut rand = [0u8; 32];
+        rng_gen(&mut rand).expect("Failt to generate randomness.");
+        State {
+            pubkey: s.pubkey,
+            balance: s.balance,
+            randomness: NewRand(rand),
+        }
     }
 }
 
