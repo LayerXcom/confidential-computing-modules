0c65a7c82c904459b52dc232df3bed88c328a2f8
osuketh <dish230@gmail.com>
2019-11-25 16:56:43 +0900

    Add client

--- client/src/main.rs
+++ client/src/main.rs
@@ -1,8 +1,36 @@
+#[macro_use]
+extern crate log;
+#[macro_use]
+extern crate clap;
+
 use clap::{Arg, App, SubCommand, AppSettings, ArgMatches};
 
 mod term;
 mod config;
 
 fn main() {
+    let matches = App::new("zface")
+        .setting(AppSettings::SubcommandRequiredElseHelp)
+        .version(crate_version!())
+        .author(crate_authors!())
+        .about("Anonify's command line interface")
+        .subcommand(anonify_commands_definition())
+        .get_matches();
+
+
     println!("Hello, world!");
+}
+
+//
+// Anonify Sub Commands
+//
+
+const ANONIFY_COMMAND: &'static str = "anonify";
+
+fn subcommand_anonify(){
+
+}
+
+fn anonify_commands_definition<'a, 'b>() -> App<'a, 'b> {
+    unimplemented!();
 }
