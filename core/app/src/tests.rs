93bdb15dafd6f4d77f5fef23d501bd38f2c3e704
osuketh <dish230@gmail.com>
2019-11-27 19:20:35 +0900

    Add test_in_enclave

--- core/app/src/tests.rs
+++ core/app/src/tests.rs
@@ -1,7 +1,10 @@
 use super::*;
 use init_enclave::EnclaveDir;
 use equote::EnclaveContext;
+use anonify_types::{RawPointer, ResultStatus};
 use constants::*;
+use auto_ffi::ecall_run_tests;
+use sgx_types::*;
 
 #[test]
 fn test_get_quote() {
@@ -14,6 +17,17 @@ fn test_get_quote() {
 }
 
 #[test]
-fn test_enclave() {
+fn test_in_enclave() {
+    let mut tmp = 3;
     let enclave = EnclaveDir::new().init_enclave().unwrap();
+    let ptr = unsafe { RawPointer::new_mut(&mut tmp) };
+    let mut res = ResultStatus::Ok;
+    let ret = unsafe { ecall_run_tests(
+        enclave.geteid(),
+        &ptr as *const RawPointer,
+        &mut res,
+    ) };
+
+    assert_eq!(ret, sgx_status_t::SGX_SUCCESS);
+    assert_eq!(res, ResultStatus::Ok);
 }
