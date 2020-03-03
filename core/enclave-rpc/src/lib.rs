b3c9cecf9c3d07095a199a83a566c33f00787276
osuketh <dish230@gmail.com>
2020-03-03 14:57:12 +0900

    Add transport

--- core/enclave-rpc/src/lib.rs
+++ core/enclave-rpc/src/lib.rs
@@ -1,3 +1,8 @@
+#![no_std]
+
+#[macro_use]
+extern crate sgx_tstd as std;
+
 mod client;
 mod config;
 mod server;
--- core/enclave-rpc/src/transport.rs
+++ core/enclave-rpc/src/transport.rs
@@ -1,9 +1,46 @@
-pub trait ClientTransport {
+use std::net::TcpStream;
+use std::io::{Read, Write};
+use serde::{Serialize, Deserialize};
+use anyhow::Result;
 
+pub trait ClientTransport {
+    fn send(&mut self, request)
 }
 
 pub trait ServerTransport {
 
 }
 
-pub struct TlsTransport;
+pub struct TlsTransport<S: rustls::Session> {
+    stream: rustls::StreamOwned<S, TcpStream>,
+}
+
+impl<S: rustls::Session> TlsTransport<S> {
+    pub fn new(stream: rustls::StreamOwned<S, TcpStream>) -> Self {
+        TlsTransport { stream }
+    }
+}
+
+pub struct Message<'a, T>
+where
+    T: Read + Write,
+{
+    transport: &'a mut T,
+    max_frame_len: u64,
+}
+
+impl<T> Message<'_, T>
+where
+    T: Read + Write,
+{
+    pub fn new(transport: &'_ mut T) -> Message<'_, T> {
+        Message {
+            transport,
+            max_frame_len: 8 * 1_024 * 1_024,
+        }
+    }
+
+    // pub fn read<D>(&mut self) -> Result<D> {
+
+    // }
+}
