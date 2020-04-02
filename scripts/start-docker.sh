37e07a3254457120e3c3ad0b9f2488a49821979c
osuketh <dish230@gmail.com>
2020-04-02 12:01:37 +0900

    fix driver from isgx to sgx

--- scripts/start-docker.sh
+++ scripts/start-docker.sh
@@ -2,5 +2,5 @@
 
 set -e
 
-docker run -v `pwd`:/root/anonify --device /dev/isgx --net=test-network --name sgx --rm -it osuketh/anonify
+docker run -v `pwd`:/root/anonify --device /dev/sgx --net=test-network --name sgx --rm -it osuketh/anonify
 LD_LIBRARY_PATH=/opt/intel/libsgx-enclave-common/aesm /opt/intel/libsgx-enclave-common/aesm/aesm_service
