20ded020a34c686555b535b275cc88fe3be3033c
osuketh <dish230@gmail.com>
2020-04-06 22:25:47 +0900

    Fix README and docker files

---
+++ docker/README.md
@@ -0,0 +1,7 @@
+# Anonify docker files
+
+In the project root directory, you can build docker files by following commands.
+```
+$ docker build -t anonify -f docker/dev.Dockerfile ./
+$ docker build -t anonify-server -f docker/server.Dockerfile ./
+```
--- scripts/start-docker.sh
+++ scripts/start-docker.sh
@@ -2,5 +2,9 @@
 
 set -e
 
+# The SDK Driver creates a device at `/dev/sgx`, non-DCAP systems using IAS.
 docker run -v `pwd`:/root/anonify --device /dev/sgx --net=test-network --name sgx --rm -it osuketh/anonify
+SGX_MODE=HW
+# After entering docker container, the very first thing is to start aesm service daemon.
 LD_LIBRARY_PATH=/opt/intel/libsgx-enclave-common/aesm /opt/intel/libsgx-enclave-common/aesm/aesm_service
+
