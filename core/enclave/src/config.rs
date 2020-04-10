6cc4afb094eff8c513c3150fcc512b57b5065164
osuketh <dish230@gmail.com>
2020-04-10 10:46:07 +0900

    Fix integration test

--- core/enclave/src/config.rs
+++ core/enclave/src/config.rs
@@ -2,4 +2,4 @@ pub const MY_ROSTER_IDX: usize = 0;
 pub const MAX_ROSTER_IDX: usize = 2;
 pub const TEST_SPID: &str = "2C149BFC94A61D306A96211AED155BE9";
 pub const UNTIL_ROSTER_IDX: usize = 10;
-pub const UNTIL_EPOCH: usize = 10;
+pub const UNTIL_EPOCH: usize = 30;
--- core/host/src/tests.rs
+++ core/host/src/tests.rs
@@ -34,6 +34,8 @@ fn test_in_enclave() {
 
 #[test]
 fn test_integration_eth_transfer() {
+    env::set_var("MY_ROSTER_IDX", "0");
+    env::set_var("MAX_ROSTER_IDX", "2");
     let enclave = EnclaveDir::new().init_enclave(true).unwrap();
     let eid = enclave.geteid();
     let mut csprng: OsRng = OsRng::new().unwrap();
