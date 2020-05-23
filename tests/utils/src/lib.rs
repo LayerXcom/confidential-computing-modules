a1f84d26fc243b404b77b903764a49d00edb2ad0
cipepser <respepic@gmail.com>
2020-05-23 09:31:36 +0900

    clean up run_inventory_tests

--- tests/utils/src/lib.rs
+++ tests/utils/src/lib.rs
@@ -7,19 +7,17 @@ inventory::collect!(TestCase);
 #[macro_export]
 macro_rules! run_inventory_tests {
      ($predicate:expr) => {{
-         // anonify_test_utils::test_start();
-         // let mut ntestcases: u64 = 0u64;
-         // let mut failurecases: Vec<String> = Vec::new();
+         anonify_test_utils::test_start();
+         let mut ntestcases: u64 = 0u64;
+         let mut failurecases: Vec<String> = Vec::new();
 
-         // for t in inventory::iter::<anonify_test_utils::TestCase>.into_iter() {
-         for t in inventory::iter::<TestCase>.into_iter() {
+         for t in inventory::iter::<anonify_test_utils::TestCase>.into_iter() {
              if $predicate(&t.0) {
-                 // anonify_test_utils::test(&mut ntestcases, &mut failurecases, t.1, &t.0);
-                 test(t.1, &t.0);
+                 anonify_test_utils::test(&mut ntestcases, &mut failurecases, t.1, &t.0);
              }
          }
 
-         // anonify_test_utils::test_end(ntestcases, failurecases)
+         anonify_test_utils::test_end(ntestcases, failurecases)
      }};
      () => {
          run_inventory_tests!(|_| true);
@@ -27,12 +25,11 @@ macro_rules! run_inventory_tests {
  }
 
 #[allow(clippy::print_literal)]
-pub fn test<F, R>(f: F, name: &str)
-// pub fn test<F, R>(ncases: &mut u64, failurecases: &mut Vec<String>, f: F, name: &str)
+pub fn test<F, R>(ncases: &mut u64, failurecases: &mut Vec<String>, f: F, name: &str)
     where
         F: FnOnce() -> R + std::panic::UnwindSafe,
 {
-    // *ncases += 1;
+    *ncases += 1;
     let t = || {
         f();
     };
@@ -40,6 +37,39 @@ pub fn test<F, R>(f: F, name: &str)
         println!("{} {} ... {}!", "testing", name, "\x1B[1;32mok\x1B[0m");
     } else {
         println!("{} {} ... {}!", "testing", name, "\x1B[1;31mfailed\x1B[0m");
-        // failurecases.push(String::from(name));
+        failurecases.push(String::from(name));
     }
+}
+
+pub fn test_start() {
+    println!("\nstart running tests");
+}
+
+pub fn test_end(ntestcases: u64, failurecases: Vec<String>) -> bool {
+    let ntotal = ntestcases as usize;
+    let nsucc = ntestcases as usize - failurecases.len();
+
+    if !failurecases.is_empty() {
+        print!("\nfailures: ");
+        println!(
+            "    {}",
+            failurecases
+                .iter()
+                .fold(String::new(), |s, per| s + "\n    " + per)
+        );
+    }
+
+    if ntotal == nsucc {
+        print!("\ntest result \x1B[1;32mok\x1B[0m. ");
+    } else {
+        print!("\ntest result \x1B[1;31mFAILED\x1B[0m. ");
+    }
+
+    println!(
+        "{} tested, {} passed, {} failed",
+        ntotal,
+        nsucc,
+        ntotal - nsucc
+    );
+    failurecases.is_empty()
 }
