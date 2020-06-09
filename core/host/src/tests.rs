7da802201dc34b3fa75e84d2dc2509ca76d43f58
cipepser <respepic@gmail.com>
2020-06-09 16:24:44 +0900

    clean up enclave_tests

--- core/host/src/tests.rs
+++ core/host/src/tests.rs
@@ -12,7 +12,7 @@ use anonify_event_watcher::{
     eventdb::{EventDB, BlockNumDB},
     eth::*,
 };
-use crate::auto_ffi::ecall_run_tests;
+use crate::auto_ffi::{ecall_run_tests, ecall_run_tests_new};
 use crate::init_enclave::EnclaveDir;
 use crate::dispatcher::*;
 
@@ -21,24 +21,8 @@ const ANONYMOUS_ASSET_ABI_PATH: &str = "../../build/Anonify.abi";
 
 #[test]
 fn test_in_enclave() {
-    let mut tmp = 3;
     let enclave = EnclaveDir::new().init_enclave(true).unwrap();
-    let ptr = unsafe { RawPointer::new_mut(&mut tmp) };
-    let mut res = ResultStatus::Ok;
     let ret = unsafe { ecall_run_tests(
-        enclave.geteid(),
-        &ptr as *const RawPointer,
-        &mut res,
-    ) };
-
-    assert_eq!(ret, sgx_status_t::SGX_SUCCESS);
-    assert_eq!(res, ResultStatus::Ok);
-}
-
-#[test]
-fn test_in_enclave_new() {
-    let enclave = EnclaveDir::new().init_enclave(true).unwrap();
-    let ret = unsafe { ecall_run_tests_new(
         enclave.geteid(),
     ) };
     assert_eq!(ret, sgx_status_t::SGX_SUCCESS);
