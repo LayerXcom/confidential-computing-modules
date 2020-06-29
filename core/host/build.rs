677be09f76f64f330176d198c4b7f503b1b60a6c
osuketh <dish230@gmail.com>
2020-06-29 17:56:04 +0900

    Fix build.rs in host

--- core/host/build.rs
+++ core/host/build.rs
@@ -8,8 +8,8 @@ fn main () {
         .unwrap_or_else(|_| format!("{}/sgx", dirs::home_dir().unwrap().display()));
     let is_sim = env::var("SGX_MODE").unwrap_or_else(|_| "HW".to_string());
 
-    // println!("cargo:rustc-link-search=native=../lib");
-    // println!("cargo:rustc-link-lib=static=Enclave_u");
+    println!("cargo:rustc-link-search=native=../lib");
+    println!("cargo:rustc-link-lib=static=Enclave_u");
 
     println!("cargo:rustc-link-search=native={}/lib64", sdk_dir);
     match is_sim.as_ref() {
