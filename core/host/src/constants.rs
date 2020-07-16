e54289c8aea219a8f59a65e4193ca3f1cf16e61f
osuketh <dish230@gmail.com>
2020-07-16 12:43:38 +0900

    Fix OUTPUT_MAX_LEN

--- core/host/src/constants.rs
+++ core/host/src/constants.rs
@@ -5,4 +5,4 @@ pub const ENCLAVE_FILE: &'static str = "enclave.signed.so";
 pub const DEV_HOSTNAME: &str = "api.trustedservices.intel.com";
 pub const HTTPS_PORT: u16 = 443;
 
-pub const OUTPUT_MAX_LEN: usize = 1024;
+pub const OUTPUT_MAX_LEN: usize = 2048;
