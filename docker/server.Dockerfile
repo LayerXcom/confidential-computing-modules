2a85e4b1a89627727972463f4ed5f3598ea1013f
osuketh <dish230@gmail.com>
2020-09-16 19:11:42 +0900

    add BUILD_DIR_FROM_HOST

--- docker/server.Dockerfile
+++ docker/server.Dockerfile
@@ -37,10 +37,10 @@ RUN source /opt/sgxsdk/environment && \
 FROM baiduxlab/sgx-rust:1804-1.1.2
 LABEL maintainer="osuke.sudo@layerx.co.jp"
 
-WORKDIR /root/anonify/example/erc20/server
-COPY --from=builder /root/anonify/contract-build/Anonify.abi ../../../contract-build/
-COPY --from=builder /root/anonify/contract-build/Anonify.bin ../../../contract-build/
-COPY --from=builder /root/anonify/.anonify/enclave.signed.so ../../../.anonify/enclave.signed.so
-COPY --from=builder /root/anonify/target/debug/erc20-server ../../../target/debug/
+WORKDIR /root/anonify
+COPY --from=builder /root/anonify/contract-build/Anonify.abi ./contract-build/
+COPY --from=builder /root/anonify/contract-build/Anonify.bin ./contract-build/
+COPY --from=builder /root/anonify/.anonify/enclave.signed.so ./.anonify/enclave.signed.so
+COPY --from=builder /root/anonify/target/debug/erc20-server ./target/debug/
 
 CMD ["./target/debug/erc20-server"]
--- frame/host/build.rs
+++ frame/host/build.rs
@@ -6,6 +6,7 @@ fn main () {
     let rust_sgx_sdk = env::var("SGX_SDK_RUST")
         .unwrap_or_else(|_| format!("{}/sgx", dirs::home_dir().unwrap().display()));
     let is_sim = env::var("SGX_MODE").unwrap_or_else(|_| "HW".to_string());
+    let build_dir = env::var("BUILD_DIR_FROM_HOST").unwrap_or_else(|_| "../../build".to_string());
 
     println!("cargo:rustc-link-search=native={}/lib64", sdk_dir);
     match is_sim.as_ref() {
@@ -21,18 +22,20 @@ fn main () {
     }
 
     let edl = format!("{}/edl", rust_sgx_sdk);
+    let test_u_c_path = format!("{}/Anonify_test_u.c", build_dir);
+    let common_u_c_path = format!("{}/Anonify_common_u.c", build_dir);
 
     match env::var("TEST") {
         Ok(test_var) if test_var == "1" => {
             cc::Build::new()
-                .file("../../build/Anonify_test_u.c")
+                .file(test_u_c_path)
                 .include("/opt/sgxsdk/include")
                 .include(edl)
                 .compile("libAnonify_test_u");
         },
         _ => {
             cc::Build::new()
-                .file("../../build/Anonify_common_u.c")
+                .file(common_u_c_path)
                 .include("/opt/sgxsdk/include")
                 .include(edl)
                 .compile("libAnonify_common_u");
