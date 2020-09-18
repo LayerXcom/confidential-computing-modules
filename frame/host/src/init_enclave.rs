f931ec2dccabceff9b6993888b287cdd726ddac1
osuketh <dish230@gmail.com>
2020-09-18 15:55:51 +0900

    init

--- frame/host/src/init_enclave.rs
+++ frame/host/src/init_enclave.rs
@@ -12,7 +12,7 @@ pub struct EnclaveDir(PathBuf);
 impl EnclaveDir {
     pub fn new() -> Self {
         let pj_root_dir = env::var("PJ_ROOT_DIR")
-            .unwrap_or_else(|_| format!("{}", dirs::home_dir().unwrap().into_os_string().to_str().unwrap()));
+            .unwrap_or_else(|_| format!("{}/anonify", dirs::home_dir().unwrap().into_os_string().to_str().unwrap()));
         let enclave_dir = PathBuf::from(pj_root_dir)
             .join(ENCLAVE_DIR);
 println!("enclave_dir: {:?}", enclave_dir);
