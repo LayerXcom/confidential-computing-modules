e3ff7ef72e30c3f25d91a1b6159d856bf4f6d36a
osuketh <dish230@gmail.com>
2020-09-19 13:57:41 +0900

    Impl

--- example/erc20/server/src/store_path_secrets.rs
+++ example/erc20/server/src/store_path_secrets.rs
@@ -1,18 +1,51 @@
 use std::fs;
 use frame_common::crypto::ExportPathSecret;
+use frame_host::PJ_ROOT_DIR;
 use failure::Error;
 
-pub struct StorePathSecret(ExportPathSecret);
+const PATH_SECRETS_DIR: &str = "path-secrets";
 
-impl StorePathSecret {
-    pub fn new(eps: ExportPathSecret) -> Self {
-        StorePathSecret(eps)
-    }
-
-    pub fn store_to_local_filesystem(self) -> Result<(), Error> {
-        let uuid = uuid::Uuid::new_v4();
-        
-        unimplemented!();
-    }
+pub struct StorePathSecrets {
+    local_path: PathBuf,
 }
 
+impl StorePathSecrets {
+    pub fn new() -> Self {
+        StorePathSecret {
+            local_path: *PJ_ROOT_DIR,
+        }
+    }
+
+    pub fn save_to_local_filesystem(self, eps: &ExportPathSecret) -> Result<(), Error> {
+        let path = self.local_path.join(PATH_SECRETS_DIR);
+        let mut file = Self::create_new_file(path)?;
+        serde_json::to_writer(&mut file, &pes)?;
+        file.flush()?;
+        file.sync_all()?;
+
+        Ok(())
+    }
+
+    #[cfg(unix)]
+    fn create_new_file(path: &Path) -> Result<fs::File> {
+        use std::os::unix::fs::OpenOptionsExt;
+
+        let file = fs::OpenOptions::new()
+            .write(true)
+            .create_new(true)
+            .mode(0o660) // Owner's read & write permission
+            .open(path)?;
+
+        Ok(file)
+    }
+
+    #[cfg(not(unix))]
+    fn create_new_file(path: &Path) -> Result<fs::File> {
+        let file = fs::OpenOptions::new()
+            .write(true)
+            .create_new(true)
+            .open(path)?;
+
+        Ok(file)
+    }
+}
