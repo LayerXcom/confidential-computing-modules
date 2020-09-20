a892d98a54f373887f1a80dba60b61fdc2968552
osuketh <dish230@gmail.com>
2020-09-20 13:50:13 +0900

    add #[allow(clippy::cast_ptr_alignment)]

--- frame/types/src/types.rs
+++ frame/types/src/types.rs
@@ -77,6 +77,7 @@ impl UntrustedStatus {
 pub struct EnclaveState(pub *const u8);
 
 impl EnclaveState {
+    #[allow(clippy::cast_ptr_alignment)]
     pub fn as_bytes(&self) -> Box<[u8]> {
         let raw_state = self.0 as *mut Box<[u8]>;
         let box_state = unsafe { Box::from_raw(raw_state) };
