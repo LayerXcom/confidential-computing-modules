62e3f6894322052f321ed51c8614202d3d9b808e
osuketh <dish230@gmail.com>
2020-10-07 21:13:56 +0900

    Add set_server_env_vars

--- example/erc20/server/src/tests.rs
+++ example/erc20/server/src/tests.rs
@@ -5,6 +5,7 @@ use integration_tests::set_env_vars;
 #[actix_rt::test]
 async fn test_deploy_post() {
     set_env_vars();
+    set_server_env_vars();
 
     // Enclave must be initialized in main function.
     let enclave = EnclaveDir::new()
@@ -22,4 +23,13 @@ async fn test_deploy_post() {
     let req = test::TestRequest::post().uri("/api/v1/deploy").to_request();
     let resp = test::call_service(&mut app, req).await;
     println!("response: {:?}", resp);
+}
+
+fn set_server_env_vars() {
+    env::set_var("ETH_URL", "http://172.28.0.2:8545");
+    env::set_var("ABI_PATH", "../../../contract-build/Anonify.abi");
+    env::set_var("BIN_PATH", "../../../contract-build/Anonify.bin");
+    env::set_var("CONFIRMATIONS", "0");
+    env::set_var("ACCOUNT_INDEX", "0");
+    env::set_var("PASSWORD", "anonify0101");
 }
