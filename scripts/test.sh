0e923189fba7555247f711d62e70380da52e201c
osuketh <dish230@gmail.com>
2020-11-02 18:10:08 +0900

    more sleep

--- scripts/test.sh
+++ scripts/test.sh
@@ -26,13 +26,13 @@ RUST_BACKTRACE=1 RUST_LOG=debug cargo test -- --nocapture
 
 cd ../../example/erc20/server
 RUST_BACKTRACE=1 RUST_LOG=debug cargo test test_deploy_post -- --nocapture
-sleep 1
+sleep 3
 RUST_BACKTRACE=1 RUST_LOG=debug cargo test test_multiple_messages -- --nocapture
-sleep 1
+sleep 3
 RUST_BACKTRACE=1 RUST_LOG=debug cargo test test_skip_invalid_event -- --nocapture
-sleep 1
+sleep 3
 RUST_BACKTRACE=1 RUST_LOG=debug cargo test test_node_recovery -- --nocapture
-sleep 1
+sleep 3
 RUST_BACKTRACE=1 RUST_LOG=debug cargo test test_join_group_then_handshake -- --nocapture
 
 echo "Unit testing..."
