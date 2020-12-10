1653061f1a79c0991a670c99e0330c40dec8b92a
cipepser <respepic@gmail.com>
2020-12-10 14:10:03 +0900

    fix cfg for SealedPathSecret

--- frame/treekem/src/lib.rs
+++ frame/treekem/src/lib.rs
@@ -54,6 +54,7 @@ mod test_funcs;
 pub use crate::application::AppKeyChain;
 pub use crate::crypto::dh::{DhPrivateKey, DhPubKey};
 pub use crate::crypto::ecies::EciesCiphertext;
+#[cfg(all(feature = "sgx", no_std))]
 pub use crate::crypto::secrets::SealedPathSecret;
 #[cfg(feature = "sgx")]
 pub use crate::crypto::secrets::{PathSecret, UnsealedPathSecret};
