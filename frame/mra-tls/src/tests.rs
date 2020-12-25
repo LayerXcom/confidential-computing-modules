504fcafc23bcb30483611cbec14a5c530b24a1f4
osuketh <dish230@gmail.com>
2020-12-25 23:03:40 +0900

    rm test_parse_cert

--- frame/mra-tls/src/tests.rs
+++ frame/mra-tls/src/tests.rs
@@ -19,10 +19,7 @@ static SERVER_ADDRESS: Lazy<String> = Lazy::new(|| {
 const LISTEN_ADDRESS: &str = "0.0.0.0:12345";
 
 pub fn run_tests() -> bool {
-    check_all_passed!(
-        run_tests!(test_request_response, test_invalid_root_cert_failed,),
-        crate::key::tests::run_tests(),
-    )
+    check_all_passed!(run_tests!(test_request_response,),)
 }
 
 #[derive(Default, Clone)]
@@ -45,12 +42,10 @@ fn test_request_response() {
         AttestedTlsConfig::new_by_ra(&spid, &ias_url, &sub_key, IAS_ROOT_CERT.to_vec()).unwrap();
 
     start_server(attested_tls_config.clone(), IAS_ROOT_CERT.to_vec());
-
     let client_config = ClientConfig::from_attested_tls_config(attested_tls_config)
         .unwrap()
         .set_attestation_report_verifier(IAS_ROOT_CERT.to_vec(), *ENCLAVE_MEASUREMENT);
     let mut client = Client::new(&*SERVER_ADDRESS, client_config).unwrap();
-
     let msg = r#"{
         "message": "Hello test_request_response"
     }"#;
