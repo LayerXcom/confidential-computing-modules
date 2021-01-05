2363a3805d23717504609cccca60174c595273ec
osuketh <dish230@gmail.com>
2021-01-05 17:18:39 +0900

    fix test.sh

--- scripts/test.sh
+++ scripts/test.sh
@@ -15,7 +15,7 @@ solc -o contract-build --bin --abi --optimize --overwrite contracts/Anonify.sol
 cd frame/types
 cargo build
 
-# Testings
+# Generate each signed.so and measurement.txt
 
 echo "Integration testing..."
 cd ${ANONIFY_ROOT}/scripts
@@ -26,8 +26,12 @@ make DEBUG=1 ENCLAVE_DIR=example/erc20/enclave
 export BACKUP=disable
 make DEBUG=1 ENCLAVE_DIR=example/erc20/enclave
 
+# Module Tests
+
 cd ${ANONIFY_ROOT}/tests/integration
 RUST_BACKTRACE=1 RUST_LOG=debug cargo test -- --nocapture
+
+# ERC20 Application Tests
 
 cd ${ANONIFY_ROOT}/example/erc20/server
 RUST_BACKTRACE=1 RUST_LOG=debug cargo test test_deploy_post -- --nocapture
@@ -40,8 +44,13 @@ RUST_BACKTRACE=1 RUST_LOG=debug cargo test test_node_recovery -- --nocapture
 sleep 1
 RUST_BACKTRACE=1 RUST_LOG=debug cargo test test_join_group_then_handshake -- --nocapture
 
+# Secret Backup Application Tests
+
+export ENCLAVE_PKG_NAME=secret_backup
 cd ${ANONIFY_ROOT}/example/secret-backup/server
 RUST_BACKTRACE=1 RUST_LOG=debug cargo test test_backup_path_secret -- --nocapture
+
+# Unit Tests
 
 echo "Unit testing..."
 export ENCLAVE_PKG_NAME=units
@@ -52,15 +61,7 @@ make DEBUG=1 TEST=1 ENCLAVE_DIR=tests/units/enclave
 cd ${ANONIFY_ROOT}
 RUST_BACKTRACE=1 RUST_LOG=debug TEST=1 cargo test -p unit-tests-host -p anonify-eth-driver -p frame-runtime -- --nocapture
 
-# Buildings
+# Compile Checks
 
 export ANONIFY_URL=http://172.28.1.1:8080
 ./scripts/build-cli.sh
-
-echo "Building ERC20 server..."
-cd example/erc20/server
-RUST_BACKTRACE=1 RUST_LOG=debug cargo build
-
-echo "Building secret-backup server..."
-cd ../../secret-backup/server
-RUST_BACKTRACE=1 RUST_LOG=debug cargo build
