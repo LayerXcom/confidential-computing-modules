b6f359db7832936546630bd94f6d4d99e53d38f6
osuketh <dish230@gmail.com>
2021-01-07 12:56:16 +0900

    set jobs to 1

--- scripts/test.sh
+++ scripts/test.sh
@@ -38,7 +38,7 @@ RUST_BACKTRACE=1 RUST_LOG=debug cargo test -- --nocapture
 # ERC20 Application Tests
 
 cd ${ANONIFY_ROOT}/example/erc20/server
-RUST_BACKTRACE=1 RUST_LOG=debug cargo test -- --nocapture
+RUST_BACKTRACE=1 RUST_LOG=debug cargo test -j 1 -- --nocapture
 # RUST_BACKTRACE=1 RUST_LOG=debug cargo test test_deploy_post -- --nocapture
 # sleep 1
 # RUST_BACKTRACE=1 RUST_LOG=debug cargo test test_multiple_messages -- --nocapture
