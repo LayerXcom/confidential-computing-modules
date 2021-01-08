b05e8e4a96ef4503a98c0fc3f3590336a7083632
osuketh <dish230@gmail.com>
2021-01-08 14:39:47 +0900

    Add name and trace

--- frame/retrier/src/retry.rs
+++ frame/retrier/src/retry.rs
@@ -1,25 +1,35 @@
 use crate::localstd::{result::Result, thread, time::Duration};
+use tracing::warn;
 
-pub struct Retry<I: Iterator<Item = Duration>> {
+pub struct Retry<'a, I: Iterator<Item = Duration>> {
     tries: usize,
     strategy: I,
+    name: &'a str,
 }
 
-impl<I: Iterator<Item = Duration>> Retry<I> {
-    pub fn new(tries: usize, strategy: I) -> Self {
-        Self { tries, strategy }
+impl<'a, I: Iterator<Item = Duration>> Retry<'a, I> {
+    pub fn new(tries: usize, strategy: I, name: &'a str) -> Self {
+        Self {
+            tries,
+            strategy,
+            name,
+        }
     }
 
     pub fn spawn<O, T, E>(self, mut operation: O) -> Result<T, E>
     where
         O: FnMut() -> Result<T, E>,
     {
-        let mut iterator = self.strategy.take(self.tries).into_iter();
+        let mut iterator = self.strategy.take(self.tries).into_iter().enumerate();
         loop {
             match operation() {
                 Ok(value) => return Ok(value),
                 Err(err) => {
-                    if let Some(delay) = iterator.next() {
+                    if let Some((curr_tries, delay)) = iterator.next() {
+                        warn!(
+                            "The {} operation retries {} times...",
+                            self.name, curr_tries
+                        );
                         thread::sleep(delay);
                     } else {
                         return Err(err);
@@ -39,7 +49,7 @@ mod tests {
     #[test]
     fn test_fixed_delay_strategy_success() {
         let mut counter = 1..=4;
-        let res = Retry::new(4, strategy::FixedDelay::new(10))
+        let res = Retry::<'_>::new(4, strategy::FixedDelay::new(10), "test_counter_success")
             .spawn(|| match counter.next() {
                 Some(c) if c == 4 => Ok(c),
                 Some(_) => Err("Not 4"),
@@ -54,11 +64,14 @@ mod tests {
     #[test]
     fn test_fixed_delay_strategy_error() {
         let mut counter = 1..=5;
-        let res = Retry::new(3, strategy::FixedDelay::new(10)).spawn(|| match counter.next() {
-            Some(c) if c == 5 => Ok(c),
-            Some(_) => Err("Some: Not 4"),
-            None => Err("None: Not 4"),
-        });
+        let res =
+            Retry::<'_>::new(3, strategy::FixedDelay::new(10), "test_counter_error").spawn(|| {
+                match counter.next() {
+                    Some(c) if c == 5 => Ok(c),
+                    Some(_) => Err("Some: Not 4"),
+                    None => Err("None: Not 4"),
+                }
+            });
 
         assert_eq!(res, Err("Some: Not 4"));
     }
