f940725cc8884848bc1b58d65ee39710b6c21ccd
osuketh <dish230@gmail.com>
2021-01-18 00:50:59 +0900

    move ENCLAVE_SIGNED_SO

--- tests/units/enclave/src/lib.rs
+++ tests/units/enclave/src/lib.rs
@@ -12,7 +12,7 @@ use once_cell::sync::Lazy;
 
 static ENABLE_BACKTRACE: Lazy<()> = Lazy::new(|| {
     backtrace::enable_backtrace(
-        &*anonify_config::ENCLAVE_SIGNED_SO,
+        &*frame_config::ENCLAVE_SIGNED_SO,
         backtrace::PrintFormat::Short,
     )
     .unwrap();
