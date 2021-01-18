d679c309e5d4751be98e386f666fdd67be593aba
osuketh <dish230@gmail.com>
2021-01-18 20:46:25 +0900

    fix CONNECTED_ENCLAVE_MEASUREMENT

--- modules/key-vault-enclave/src/server.rs
+++ modules/key-vault-enclave/src/server.rs
@@ -1,7 +1,7 @@
 use crate::handlers::KeyVaultHandler;
 use anonify_io_types::*;
 use frame_common::state_types::StateType;
-use frame_config::{CONNECTED_ENCLAVE_MEASUREMENT, IAS_ROOT_CERT};
+use frame_config::{ANONIFY_ENCLAVE_MEASUREMENT, IAS_ROOT_CERT};
 use frame_enclave::EnclaveEngine;
 use frame_mra_tls::{AttestedTlsConfig, Server, ServerConfig};
 use frame_runtime::traits::*;
@@ -29,10 +29,7 @@ impl EnclaveEngine for ServerStarter {
             AttestedTlsConfig::new_by_ra(&spid, &ias_url, &sub_key, IAS_ROOT_CERT.to_vec())?;
 
         let server_config = ServerConfig::from_attested_tls_config(attested_tls_config)?
-            .set_attestation_report_verifier(
-                IAS_ROOT_CERT.to_vec(),
-                *CONNECTED_ENCLAVE_MEASUREMENT,
-            );
+            .set_attestation_report_verifier(IAS_ROOT_CERT.to_vec(), *ANONIFY_ENCLAVE_MEASUREMENT);
 
         let mut server = Server::new(SERVER_ADDRESS.to_string(), server_config);
         let store_path_secrets = enclave_context.store_path_secrets();
