d7303d14858a41c49ca7c673b46c4ce4efb03173
osuketh <dish230@gmail.com>
2021-01-23 14:53:39 +0900

    fix secretbackup test

--- example/secret-backup/server/src/tests.rs
+++ example/secret-backup/server/src/tests.rs
@@ -1,10 +1,12 @@
 use crate::{api, handlers::*, Server as KeyVaultServer};
 use actix_web::{test, web, App};
+use anonify_ecall_types::input;
 use anonify_eth_driver::eth::{EthDeployer, EthSender, EventWatcher};
 use codec::{Decode, Encode};
 use erc20_server::{handlers::*, Server as ERC20Server};
 use erc20_state_transition::construct;
 use ethabi::Contract as ContractABI;
+use frame_common::crypto::Ed25519ChallengeResponse;
 use frame_config::PJ_ROOT_DIR;
 use frame_host::EnclaveDir;
 use frame_runtime::primitives::U64;
@@ -24,6 +26,7 @@ use web3::{
     types::Address,
     Web3,
 };
+use serde_json::json;
 
 #[actix_rt::test]
 async fn test_backup_path_secret() {
@@ -781,29 +784,29 @@ async fn verify_encrypting_key<P: AsRef<Path>>(
 }
 
 fn init_100_req(enc_key: &DhPubKey) -> erc20_api::state::post::Request {
-    let init_100 = construct {
-        total_supply: U64::from_raw(100),
-    };
-    let enc_cmd = EciesCiphertext::encrypt(&enc_key, init_100.encode()).unwrap();
+    let sig = [
+        236, 103, 17, 252, 166, 199, 9, 46, 200, 107, 188, 0, 37, 111, 83, 105, 175, 81, 231, 14,
+        81, 100, 221, 89, 102, 172, 30, 96, 15, 128, 117, 146, 181, 221, 149, 206, 163, 208, 113,
+        198, 241, 16, 150, 248, 99, 170, 85, 122, 165, 197, 14, 120, 110, 37, 69, 32, 36, 218, 100,
+        64, 224, 226, 99, 2,
+    ];
+    let pubkey = [
+        164, 189, 195, 42, 48, 163, 27, 74, 84, 147, 25, 254, 16, 14, 206, 134, 153, 148, 33, 189,
+        55, 149, 7, 15, 11, 101, 106, 28, 48, 130, 133, 143,
+    ];
+    let challenge = [
+        244, 158, 183, 202, 237, 236, 27, 67, 39, 95, 178, 136, 235, 162, 188, 106, 52, 56, 6, 245,
+        3, 101, 33, 155, 58, 175, 168, 63, 73, 125, 205, 225,
+    ];
+    let access_policy = Ed25519ChallengeResponse::new_from_bytes(sig, pubkey, challenge);
+    let init_100 = json!({
+        "total_supply": U64::from_raw(100),
+    });
+    let req = input::Command::new(access_policy, init_100, "construct");
+    let encrypted_req =
+        EciesCiphertext::encrypt(&enc_key, serde_json::to_vec(&req).unwrap()).unwrap();
 
-    erc20_api::state::post::Request {
-        function: "construct".to_string(),
-        sig: [
-            236, 103, 17, 252, 166, 199, 9, 46, 200, 107, 188, 0, 37, 111, 83, 105, 175, 81, 231,
-            14, 81, 100, 221, 89, 102, 172, 30, 96, 15, 128, 117, 146, 181, 221, 149, 206, 163,
-            208, 113, 198, 241, 16, 150, 248, 99, 170, 85, 122, 165, 197, 14, 120, 110, 37, 69, 32,
-            36, 218, 100, 64, 224, 226, 99, 2,
-        ],
-        pubkey: [
-            164, 189, 195, 42, 48, 163, 27, 74, 84, 147, 25, 254, 16, 14, 206, 134, 153, 148, 33,
-            189, 55, 149, 7, 15, 11, 101, 106, 28, 48, 130, 133, 143,
-        ],
-        challenge: [
-            244, 158, 183, 202, 237, 236, 27, 67, 39, 95, 178, 136, 235, 162, 188, 106, 52, 56, 6,
-            245, 3, 101, 33, 155, 58, 175, 168, 63, 73, 125, 205, 225,
-        ],
-        encrypted_command: enc_cmd,
-    }
+    erc20_api::state::post::Request { encrypted_req }
 }
 
 const BALANCE_OF_REQ: erc20_api::state::get::Request = erc20_api::state::get::Request {
