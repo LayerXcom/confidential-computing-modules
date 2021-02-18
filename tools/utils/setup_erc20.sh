de5baa97613f525e456dffa4f6ffa07eef91a6d4
cipepser <respepic@gmail.com>
2021-02-18 20:01:59 +0900

    setup_erc20.sh

--- tools/utils/setup_erc20.sh
+++ tools/utils/setup_erc20.sh
@@ -2,22 +2,36 @@
 
 export ANONIFY_URL=http://172.38.1.1:8080
 
+echo 'deploying...'
 contract_address=$(curl ${ANONIFY_URL}/api/v1/deploy -k -s -X POST -H "Content-Type: application/json" -d '' | jq .contract_address | sed 's/"//g')
+echo "got contract_address: ${contract_address}"
 
+echo 'set contract_address'
 curl ${ANONIFY_URL}/api/v1/set_contract_address -k -s -X GET -H "Content-Type: application/json" -d "{\"contract_addr\":\"${contract_address}\"}"
 
+echo 'start_sync_bc...'
 curl ${ANONIFY_URL}/api/v1/start_sync_bc -k -s -X GET -H "Content-Type: application/json" -d ''
 
-curl ${ANONIFY_URL}/api/v1/enclave_encryption_key -k -s -X GET -H "Content-Type: application/json" -d '' > pubkey.json
+sleep 2;
+
+echo 'get enclave_encryption_key and save to ~/anonify/pubkey.json'
+curl ${ANONIFY_URL}/api/v1/enclave_encryption_key -k -s -X GET -H "Content-Type: application/json" -d '' > ~/anonify/pubkey.json
 
 cd ~/anonify
+echo 'enc init.json'
 ./enc ./pubkey.json ./init.json
 
+sleep 2;
+
+echo 'init_state...'
 curl ${ANONIFY_URL}/api/v1/state -k -s -X POST -H "Content-Type: application/json" -d @encrypted_init.json
 
+sleep 2;
+
+echo 'enc blob'
 ./enc ./pubkey.json ./blob.10.json
 #curl ${ANONIFY_URL}/api/v1/state -k -s -X POST -H "Content-Type: application/json" -d @encrypted_blob.10.json
 
 cp encrypted_blob.10.json /root/anonify/tools/vegeta/
-cd /root/anonify/tools/vegeta
+# cd /root/anonify/tools/vegeta
 
