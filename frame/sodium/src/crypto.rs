2077f8e75a0f9fc6e7cab799322e9b508a2d5e5a
osuketh <dish230@gmail.com>
2021-03-01 18:01:19 +0900

    fix sodium crypto

--- frame/sodium/src/crypto.rs
+++ frame/sodium/src/crypto.rs
@@ -4,6 +4,7 @@ use crate::crypto_box::{self, aead::Aead, Box as CryptoBox, PublicKey, SecretKey
 use crate::local_anyhow::{anyhow, Result};
 use crate::localstd::{fmt, vec::Vec};
 use crate::rand_core::{CryptoRng, RngCore};
+#[cfg(feature = "sgx")]
 use crate::sealing::UnsealedEnclaveDecryptionKey;
 use crate::serde::{
     de::{self, SeqAccess, Unexpected},
@@ -206,6 +207,7 @@ impl SodiumPrivateKey {
         SodiumPubKey(self.0.public_key())
     }
 
+    #[cfg(feature = "sgx")]
     pub fn try_into_sealing<'a>(&self) -> Result<Vec<u8>> {
         UnsealedEnclaveDecryptionKey::from_sodium_priv_key(&self).encoded_sealing()
     }
