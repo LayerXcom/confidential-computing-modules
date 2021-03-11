6b515ef0534d0cd9551a36c77b24879f338beb1f
osuketh <dish230@gmail.com>
2021-03-11 18:45:40 +0900

    tmp

--- ethereum/deployer/src/main.rs
+++ ethereum/deployer/src/main.rs
@@ -1,1 +1,13 @@
-fn main() {}
+use eth_deployer::EthDeployer;
+use std::env;
+
+fn main() {
+    let eth_url = env::var("ETH_URL").expect("ETH_URL is not set");
+    let account_index = env::var("ACCOUNT_INDEX")
+        .unwrap_or_else(|| "0".to_string())
+        .parse::<usize>()
+        .unwrap();
+    let password = env::var("PASSWORD").ok();
+    let deployer = EthDeployer::new(&eth_url).unwrap();
+    let signer = deployer.get_account(account_index, password).await.unwrap();
+}
