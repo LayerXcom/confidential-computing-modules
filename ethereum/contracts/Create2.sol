151f6cb2e5d3053b53463482efad9dfe6a8d35c2
osuketh <dish230@gmail.com>
2021-03-11 14:39:27 +0900

    Add Create2

---
+++ ethereum/contracts/Create2.sol
@@ -0,0 +1,10 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.7.4;
+
+import "./Anonify.sol";
+
+contract DeployAnonify {
+    function deploy(bytes32 _salt) public {
+        new Anonify{salt: _salt}();
+    }
+}
