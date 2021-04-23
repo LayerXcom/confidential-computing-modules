49baae872f637a862ff38f0b210359609344b29b
cipepser <respepic@gmail.com>
2021-04-23 14:05:02 +0900

    add tracing

--- tests/utils/src/lib.rs
+++ tests/utils/src/lib.rs
@@ -11,5 +11,11 @@ extern crate inventory;
 #[cfg(feature = "sgx")]
 pub use test_utils_proc_macro::test_case;
 
+#[cfg(feature = "std")]
+#[macro_use]
+extern crate lazy_static;
+
 #[cfg(feature = "sgx")]
 pub mod runner;
+#[cfg(feature = "std")]
+pub mod tracing;
