d4b09d4c7262b0611a9ae6705b0987e0e0f8d2a6
cipepser <respepic@gmail.com>
2021-04-26 11:44:41 +0900

    rename decode_cipher_handshake_data to decode_ciphertext_event

--- modules/anonify-eth-driver/src/eth/event_watcher.rs
+++ modules/anonify-eth-driver/src/eth/event_watcher.rs
@@ -95,7 +95,7 @@ impl fmt::LowerHex for EthLog {
 }
 
 impl EthLog {
-    fn decode_cipher_handshake_data(&self) -> Result<(Vec<u8>, StateCounter)> {
+    fn decode_ciphertext_event(&self) -> Result<(Vec<u8>, StateCounter)> {
         let tokens = ethabi::decode(&[ParamType::Bytes, ParamType::Uint(256)], &self.0.data.0)?;
         if tokens.len() != 2 {
             return Err(HostError::InvalidNumberOfEthLogToken(2));
@@ -153,10 +153,10 @@ impl Web3Logs {
 
             // Processing conditions by ciphertext or handshake event
             if log.0.topics[0] == *STORE_TREEKEM_CIPHERTEXT_EVENT {
-                let (bytes, state_counter) = match log.decode_cipher_handshake_data() {
+                let (bytes, state_counter) = match log.decode_ciphertext_event() {
                     Ok(d) => d,
                     Err(e) => {
-                        error!("decode_cipher_handshake_data: {}", e);
+                        error!("Failed decode_ciphertext_event for treekem ciphertext: {}", e);
                         continue;
                     }
                 };
@@ -164,7 +164,7 @@ impl Web3Logs {
                 let res = match TreeKemCiphertext::decode(&mut &bytes[..]) {
                     Ok(c) => c,
                     Err(e) => {
-                        error!("TreeKemCiphertext::decode: {}", e);
+                        error!("Failed TreeKemCiphertext::decode for treekem ciphertext: {}", e);
                         continue;
                     }
                 };
@@ -179,10 +179,10 @@ impl Web3Logs {
                 };
                 payloads.push(payload);
             } else if log.0.topics[0] == *STORE_TREEKEM_HANDSHAKE_EVENT {
-                let (bytes, state_counter) = match log.decode_cipher_handshake_data() {
+                let (bytes, state_counter) = match log.decode_ciphertext_event() {
                     Ok(d) => d,
                     Err(e) => {
-                        error!("decode_cipher_handshake_data: {}", e);
+                        error!("Failed decode_ciphertext_event for treekem handshake: {}", e);
                         continue;
                     }
                 };
@@ -190,7 +190,7 @@ impl Web3Logs {
                 let res = match ExportHandshake::decode(&bytes[..]) {
                     Ok(c) => c,
                     Err(e) => {
-                        error!("ExportHandshake::decode: {}", e);
+                        error!("Failed ExportHandshake::decode for treekem handshake: {}", e);
                         continue;
                     }
                 };
@@ -205,10 +205,10 @@ impl Web3Logs {
                 };
                 payloads.push(payload);
             } else if log.0.topics[0] == *STORE_ENCLAVE_KEY_CIPHERTEXT_EVENT {
-                let (bytes, state_counter) = match log.decode_cipher_handshake_data() {
+                let (bytes, state_counter) = match log.decode_ciphertext_event() {
                     Ok(d) => d,
                     Err(e) => {
-                        error!("decode_cipher_handshake_data: {}", e);
+                        error!("Failed decode_ciphertext_event for enclave_key: {}", e);
                         continue;
                     }
                 };
@@ -216,7 +216,7 @@ impl Web3Logs {
                 let res = match EnclaveKeyCiphertext::decode(&mut &bytes[..]) {
                     Ok(c) => c,
                     Err(e) => {
-                        error!("EnclaveKeyCiphertext::decode: {}", e);
+                        error!("EnclaveKeyCiphertext::decode for enclave_key: {}", e);
                         continue;
                     }
                 };
@@ -420,7 +420,7 @@ impl InnerEnclaveLog {
 
                 // Logging a skipped event
                 match (&self.logs).into_iter().find(|log| {
-                    match log.decode_cipher_handshake_data() {
+                    match log.decode_ciphertext_event() {
                         Ok((bytes, _state_counter)) => {
                             match TreeKemCiphertext::decode(&mut &bytes[..]) {
                                 Ok(res) => CommandCiphertext::TreeKem(res) == *ciphertext,
