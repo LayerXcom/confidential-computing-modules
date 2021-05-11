ea94722f42d9475d6884a990f1c0cf1c901235aa
osuketh <dish230@gmail.com>
2021-05-11 15:16:13 +0900

    fix permission settings

--- scripts/gen-enclave-config.sh
+++ scripts/gen-enclave-config.sh
@@ -6,6 +6,7 @@ dirpath=$(cd $(dirname $0) && pwd)
 cd "${dirpath}/../config"
 
 cp -f Enclave.config.xml Enclave.prd.config.xml
+sudo chmod 766 Enclave.prd.config.xml
 sed -i "" -e "s|<ProdID>0</ProdID>|<ProdID>${PROD_ID}</ProdID>|" Enclave.prd.config.xml
 sed -i "" -e "s|<ISVSVN>0</ISVSVN>|<ISVSVN>${ISVSVN}</ISVSVN>|" Enclave.prd.config.xml
 sed -i "" -e "s|<DisableDebug>0</DisableDebug>|<DisableDebug>1</DisableDebug>|" Enclave.prd.config.xml
