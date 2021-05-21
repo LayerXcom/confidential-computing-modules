884317380988fe608c204a59b5829f8fd1ef92b6
Sho Nakatani <lay.sakura@gmail.com>
2021-05-21 19:43:11 +0900

    Serialize, Deserialize for AvgState

--- modules/encrypted-sql-ops-ecall-types/src/aggregate_state/avg.rs
+++ modules/encrypted-sql-ops-ecall-types/src/aggregate_state/avg.rs
@@ -1,7 +1,9 @@
+use crate::serde::{Deserialize, Serialize};
 /// Intermediate state to calculate average.
 ///
 /// FIXME: Currently `i64` input and `f64` output is only supported.
-#[derive(Clone, PartialEq, Debug, Default)]
+#[derive(Clone, PartialEq, Debug, Default, Serialize, Deserialize)]
+#[serde(crate = "crate::serde")]
 pub struct AvgState {
     /// current total
     pub sum: i64,
