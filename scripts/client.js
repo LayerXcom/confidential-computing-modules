dfea176f6810762a549d7474f022afcdb5095464
cipepser <respepic@gmail.com>
2021-05-21 15:14:11 +0900

    add js client

---
+++ scripts/client.js
@@ -0,0 +1,104 @@
+const _sodium = require('libsodium-wrappers');
+const axios = require('axios');
+const axiosBase = require('axios');
+
+const encrypt = async ([pubkey, msg]) => {
+    await _sodium.ready;
+    const sodium = _sodium;
+
+    let pk_server_raw = Buffer.from(
+        JSON.parse(pubkey).enclave_encryption_key
+    ).toString('hex');
+    let pair_client = sodium.crypto_box_keypair();
+    let sk_client = pair_client.privateKey;
+    let pk_client = pair_client.publicKey;
+
+    let pk_server = sodium.from_hex(pk_server_raw);
+
+    let nonce = sodium.randombytes_buf(sodium.crypto_secretbox_NONCEBYTES);
+
+    let ciphertext = sodium.crypto_box_easy(msg, nonce, pk_server, sk_client);
+    const encrypted = {
+        "ciphertext": Buffer.from(ciphertext).toString('hex'),
+        "ephemeral_public_key": Buffer.from(pk_client).toString('hex'),
+        "nonce": Buffer.from(nonce).toString('hex')
+    };
+
+    return JSON.stringify(encrypted);
+};
+
+const generate_init_state_request = () => {
+    let init = {
+        "access_policy": {
+            "account_id": [216, 211, 74, 222, 192, 13, 199, 12, 37, 58, 141, 38, 252, 74, 55, 83, 152, 119, 212, 147]
+        },
+        "runtime_params": {
+            "total_supply": 100,
+        },
+        "cmd_name": "construct",
+        "counter": 1,
+    };
+    return JSON.stringify(init);
+}
+
+const generate_balance_of_request = () => {
+    let init = {
+        "access_policy": {
+            "account_id": [216, 211, 74, 222, 192, 13, 199, 12, 37, 58, 141, 38, 252, 74, 55, 83, 152, 119, 212, 147]
+        },
+        "runtime_params": {},
+        "state_name": "balance_of",
+    };
+    return JSON.stringify(init);
+}
+
+
+(async () => {
+    const axios = axiosBase.create({
+        baseURL: 'http://0.0.0.0:18550',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        responseType: 'json'
+    });
+
+    // init state
+    axios.get('/api/v1/enclave_encryption_key')
+        .then((res) => { return [JSON.stringify(res.data), generate_init_state_request()]; })
+        .catch((err) => {
+            console.log('ERROR in enclave_encryption_key: ' + err);
+            process.exit(1);
+        })
+        .then(encrypt)
+        .then(
+            (encrypted) => {
+                axios.post('/api/v1/state', encrypted)
+                    .then((res) => { return JSON.stringify(res.data); })
+                    .catch((err) => {
+                        console.log('ERROR in init_state: ' + err);
+                        process.exit(1);
+                    })
+            }
+        );
+
+    // confirm whether the state is updated or not
+    setTimeout(() => {
+        axios.get('/api/v1/enclave_encryption_key')
+            .then((res) => { return [JSON.stringify(res.data), generate_balance_of_request()]; })
+            .catch((err) => {
+                console.log('ERROR in enclave_encryption_key: ' + err);
+                process.exit(1);
+            })
+            .then(encrypt)
+            .then(async (encrypted) => {
+                const state = await axios.get('/api/v1/state', { data: encrypted, })
+                    .then((res) => { return JSON.stringify(res.data); })
+                    .catch((err) => {
+                        console.log('ERROR in balance_of: ' + err);
+                        process.exit(1);
+                    });
+                return state;
+            })
+            .then((result) => { console.log('result:' + JSON.stringify(res)) })
+    }, 1000);
+})();
--- scripts/e2e-test.sh
+++ scripts/e2e-test.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 set -e
 
-ANONIFY_TAG=v0.5.10
-DEPLOYER_TAG=v0.5.12
-STATE_RUNTIME_URL=http://0.0.0.0:18550
-ETH_URL=http://172.16.14.2:8545
+export ANONIFY_TAG=v0.5.10
+export DEPLOYER_TAG=v0.5.12
+export STATE_RUNTIME_URL=http://0.0.0.0:18550
+export ETH_URL=http://172.16.14.2:8545
+export COMPOSE_NETWORK_PREFIX=s
 CI_ROOT_DIR=$(pwd)
 
 echo "ganache is starting..."
@@ -12,17 +13,16 @@ docker-compose -f e2e-docker-compose.yml up -d ganache
 sleep 5
 
 echo "deploying factory contract..."
-FACTORY_CONTRACT_ADDRESS=$(docker run --network s_e2e_test_net -e CONFIRMATIONS=0 -e ETH_URL="$ETH_URL" --rm anonify.azurecr.io/deployer:$DEPLOYER_TAG factory)
+FACTORY_CONTRACT_ADDRESS=$(docker run --network "$COMPOSE_NETWORK_PREFIX"_e2e_test_net -e CONFIRMATIONS=0 -e ETH_URL="$ETH_URL" --rm anonify.azurecr.io/deployer:$DEPLOYER_TAG factory)
 export FACTORY_CONTRACT_ADDRESS=$FACTORY_CONTRACT_ADDRESS
 echo "FACTORY_CONTRACT_ADDRESS: ""$FACTORY_CONTRACT_ADDRESS"
 
 echo "deploying anonify contract..."
-docker run --network s_e2e_test_net -e CONFIRMATIONS=0 -e ETH_URL="$ETH_URL" --rm anonify.azurecr.io/deployer:$DEPLOYER_TAG anonify_ek "$FACTORY_CONTRACT_ADDRESS"
+docker run --network "$COMPOSE_NETWORK_PREFIX"_e2e_test_net -e CONFIRMATIONS=0 -e ETH_URL="$ETH_URL" --rm anonify.azurecr.io/deployer:$DEPLOYER_TAG anonify_ek "$FACTORY_CONTRACT_ADDRESS"
 
 echo "key_vault is starting..."
 FACTORY_CONTRACT_ADDRESS=$FACTORY_CONTRACT_ADDRESS docker-compose -f e2e-docker-compose.yml up -d key_vault
-
-sleep 10
+sleep 5
 
 echo "state_runtime_1 is starting..."
 FACTORY_CONTRACT_ADDRESS=$FACTORY_CONTRACT_ADDRESS docker-compose -f e2e-docker-compose.yml up -d state_runtime
@@ -39,3 +39,11 @@ else
   echo "failed to fetch enclave_encryption_key"
   exit 1
 fi
+
+cd "$CI_ROOT_DIR"
+npm init -y
+npm install axios
+npm install
+npm install libsodium-wrappers
+
+node ./scripts/client.js
