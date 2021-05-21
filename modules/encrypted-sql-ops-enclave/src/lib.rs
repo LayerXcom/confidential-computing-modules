f4cfdf6e4f3b0af3710403ca48a2e131f4ab76c5
Sho Nakatani <lay.sakura@gmail.com>
2021-05-21 21:13:51 +0900

    Moves AvgState to encrypted-sql-ops-enclave

--- modules/encrypted-sql-ops-enclave/src/lib.rs
+++ modules/encrypted-sql-ops-enclave/src/lib.rs
@@ -1,8 +1,9 @@
 //! Value Objects and domain services to execute SQL operations / encryption.
 //!
-//! Everything here is supposed to appear as plain text in RDBMS's process memory space.
-//! Take care not to take/pass user inputs in/to this crate.
+//! All of them are designed to be only visible in enclave.
 //!
 //! Ideally, everything in this crate serves for any RDBMS's extension development.
 
 #![deny(missing_debug_implementations, missing_docs)]
+
+pub mod aggregate;
