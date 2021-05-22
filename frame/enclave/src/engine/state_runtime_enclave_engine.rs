c8338cf905275cc19753731a7ed23c38c31e0348
Sho Nakatani <lay.sakura@gmail.com>
2021-05-22 21:46:03 +0000

    refactor: remove StateRuntimeEnclaveEngine::handle_without_runtime

--- frame/enclave/src/engine/state_runtime_enclave_engine.rs
+++ frame/enclave/src/engine/state_runtime_enclave_engine.rs
@@ -18,19 +18,11 @@ pub trait StateRuntimeEnclaveEngine: Sized + Default {
         Ok(())
     }
 
-    /// If the module have a state transition runtime, use this handler.
+    /// Handler for state transition runtime
     fn handle<R, C>(self, _enclave_context: &C, _max_mem_size: usize) -> anyhow::Result<Self::EO>
     where
         R: RuntimeExecutor<C, S = StateType>,
         C: ContextOps<S = StateType> + Clone,
-    {
-        Ok(Self::EO::default())
-    }
-
-    /// If the module doesn't have a state transition runtime, use this handler.
-    fn handle_without_runtime<C>(_enclave_context: &C) -> anyhow::Result<Self::EO>
-    where
-        C: ConfigGetter,
     {
         Ok(Self::EO::default())
     }
