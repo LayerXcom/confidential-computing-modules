454dce6b602e7b1957b3abf6c5130213aec88b2f
osuketh <dish230@gmail.com>
2021-05-26 11:52:53 +0900

    add use log::error;

--- example/key-vault/enclave/src/ecalls.rs
+++ example/key-vault/enclave/src/ecalls.rs
@@ -3,6 +3,7 @@ use anyhow::anyhow;
 use frame_enclave::{register_ecall, BasicEnclaveEngine};
 use key_vault_ecall_types::cmd::*;
 use key_vault_enclave::{context::KeyVaultEnclaveContext, workflow::*};
+use log::error;
 use std::{ptr, vec::Vec};
 
 #[allow(dead_code)]
