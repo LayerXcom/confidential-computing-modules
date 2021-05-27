ec07063b45ce2336b54951211da9089c1416ea3a
Sho Nakatani <lay.sakura@gmail.com>
2021-05-27 20:14:31 +0900

    fix: unnecessary file

--- example/encrypted-sql-ops/pg-extension/sql/func.generated.sql
+++
@@ -1,6 +0,0 @@
--- ./src/func.rs:7:0
-CREATE OR REPLACE FUNCTION "encinteger_from"("raw_integer" integer) RETURNS EncInteger STRICT LANGUAGE c AS 'MODULE_PATHNAME', 'encinteger_from_wrapper';
--- ./src/func.rs:18:0
-CREATE OR REPLACE FUNCTION "encinteger_avg_state_func"("_internal_state" AvgState, "_next_data_value" EncInteger) RETURNS AvgState STRICT LANGUAGE c AS 'MODULE_PATHNAME', 'encinteger_avg_state_func_wrapper';
--- ./src/func.rs:23:0
-CREATE OR REPLACE FUNCTION "encinteger_avg_final_func"("_internal_state" AvgState) RETURNS integer STRICT LANGUAGE c AS 'MODULE_PATHNAME', 'encinteger_avg_final_func_wrapper';
