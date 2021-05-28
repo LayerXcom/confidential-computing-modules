eee3a3d1ca5921dca1f401c8f27cb180448190fa
osuketh <dish230@gmail.com>
2021-05-28 22:10:55 +0900

    Wait until the enclave is healthy

--- scripts/occlum-host-test.sh
+++ scripts/occlum-host-test.sh
@@ -4,5 +4,19 @@ set -e
 
 ANONIFY_ROOT="$(cd $(dirname $0); pwd)/.."
 
+# Wait until the enclave is healthy
+set +e
+while true;
+do
+    grpc_health_probe -addr "$DOCKER_ENCLAVE_IP_ADDRESS:$OCCLUM_ENCLAVE_PORT"
+    if [[ $? -eq 0 ]];
+    then
+        echo "enclave is ready"
+        break;
+    fi
+    sleep 5
+done
+set -e
+
 cd "$ANONIFY_ROOT"
 cargo test -p occlum-host-node -- --nocapture
