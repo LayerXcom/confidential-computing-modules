9eaea3a45aa7ff7b59a65689329d6caefc9152b9
Sho Nakatani <lay.sakura@gmail.com>
2021-05-28 01:35:21 +0000

    fix: namespace

--- modules/encrypted-sql-ops-host/src/workflow.rs
+++ modules/encrypted-sql-ops-host/src/workflow.rs
@@ -2,38 +2,4 @@
 //!
 //! FIXME: EcallController would be better name.
 
-pub mod host_input;
-pub mod host_output;
-
-use frame_host::engine::*;
-use module_encrypted_sql_ops_ecall_types::enclave_types::{
-    EncIntegerWrapper as EnclaveEncIntegerWrapper, RawInteger as EnclaveRawInteger,
-};
-
-/// Constructor of `ENCINTEGER` custom type.
-///
-/// # Important notice
-///
-/// Encrypted type constructors are virtually vulnerable in that:
-///
-/// - Users (who writes SQLs) can INSERT (or any other DML) secret data.
-/// - SQL client and server takes the secret in plain text.
-/// - Just after SQL server passes the secret to encrypted type constructors, the secret gets encrypted inside enclave.
-///
-/// # FIXME
-///
-/// [StealthDB](https://github.com/cryptograph/stealthdb), for example, has "Client Proxy" layer
-/// to automatically encrypt secret data just after users submit an SQL.
-/// Trustful transport layer via Remote-Attestation is used to exchange encryption key.
-///
-/// We should do the similar.
-#[derive(Debug)]
-pub struct EncIntegerFromWorkflow;
-
-impl HostEngine for EncIntegerFromWorkflow {
-    type HI = host_input::RawInteger;
-    type EI = EnclaveRawInteger;
-    type EO = EnclaveEncIntegerWrapper;
-    type HO = host_output::EncIntegerWrapper;
-    const ECALL_MAX_SIZE: usize = 64;
-}
+pub mod encinteger_from;
