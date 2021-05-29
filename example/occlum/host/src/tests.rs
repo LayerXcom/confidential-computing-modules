bb0550b24a920328d14155d2d64b5c04d6717fdf
osuketh <dish230@gmail.com>
2021-05-29 21:22:18 +0900

    fix

--- example/occlum/host/src/tests.rs
+++ example/occlum/host/src/tests.rs
@@ -1,5 +1,23 @@
 use occlum_rpc_types::hello_world::{greeter_client::GreeterClient, HelloRequest};
 use std::env;
+use tonic_health::proto::{health_client::HealthClient, HealthCheckRequest};
+
+#[tokio::test]
+async fn test_health_check() -> Result<(), Box<dyn std::error::Error>> {
+    let enclave_ip =
+        env::var("OCCLUM_ENCLAVE_IP_ADDRESS").expect("OCCLUM_ENCLAVE_IP_ADDRESS is not set.");
+    let enclave_port = env::var("OCCLUM_ENCLAVE_PORT").expect("OCCLUM_ENCLAVE_PORT is not set.");
+    let mut client =
+        HealthClient::connect(format!("http://{}:{}", enclave_ip, enclave_port)).await?;
+
+    let request = tonic::Request::new(HealthCheckRequest::default());
+
+    let response = client.check(request).await?;
+
+    println!("RESPONSE={:?}", response);
+
+    Ok(())
+}
 
 #[tokio::test]
 async fn test_hello() -> Result<(), Box<dyn std::error::Error>> {
