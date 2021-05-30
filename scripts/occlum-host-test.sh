1828e74494175207f0f673b69ee89fcdc1491594
osuketh <dish230@gmail.com>
2021-05-31 01:04:34 +0900

    Add maximum tries to hc

--- scripts/occlum-host-test.sh
+++ scripts/occlum-host-test.sh
@@ -1,19 +1,28 @@
 #!/bin/bash
 
-set -ex
+set -e
 
 ANONIFY_ROOT="$(cd $(dirname $0); pwd)/.."
 
+i=0
+MAX_TRIES=40
 # Wait until the enclave is healthy
 set +e
 while true;
 do
     grpcurl -plaintext -d '{ "service": "helloworld.Greeter" }' "$OCCLUM_ENCLAVE_IP_ADDRESS:$OCCLUM_ENCLAVE_PORT" grpc.health.v1.Health/Check
-    if [[ $? -eq 0 ]];
-    then
+    if [ $? -eq 0 ]; then
         echo "enclave is ready"
         break;
     fi
+
+    ((i=i+1))
+    if [ $i -gt $MAX_TRIES ]; then
+        echo "The number of trials has exceeded the maximum." >&2
+        exit 1
+    fi
+
+    echo "Tried health check to the enclave $i times..."
     sleep 10
 done
 set -e
