dbb7d948784997e72f6c34e9f5bd76a7f0f03359
Sho Nakatani <lay.sakura@gmail.com>
2021-05-31 12:57:00 +0900

    feat: silent mode

--- docker/entrypoint/fixuid.bash
+++ docker/entrypoint/fixuid.bash
@@ -1,3 +1,10 @@
 #!/bin/bash
-[ "$DISABLE_FIXUID" = 1 ] && echo 'skip fixuid' || fixuid
+
+if [ "$DISABLE_FIXUID" = 1 ]; then
+  echo 'skip fixuid'
+else
+  echo 'Running fixuid. Wait a while...'
+  fixuid -q
+fi
+
 bash
