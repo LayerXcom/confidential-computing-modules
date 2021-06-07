e8fc964a10878b728b6847bd5540a915281b27c0
Sho Nakatani <lay.sakura@gmail.com>
2021-06-07 12:51:59 +0000

    refactor: ecall_entry_point made simpler (to be DRY with StateRuntimeEnclaveUseCase)

--- frame/enclave/src/engine/basic_enclave_use_case.rs
+++ frame/enclave/src/engine/basic_enclave_use_case.rs
@@ -1,10 +1,9 @@
-use anyhow::anyhow;
-use bincode::Options;
 use frame_common::{EnclaveInput, EnclaveOutput};
 use frame_runtime::ConfigGetter;
 use log::error;
 use serde::{de::DeserializeOwned, Serialize};
-use std::{format, ptr, vec::Vec};
+
+use super::{mk_input_ecall_entry_point, mk_output_ecall_entry_point};
 
 pub trait BasicEnclaveUseCase: Sized {
     type EI: EnclaveInput + DeserializeOwned + Default;
@@ -25,51 +24,20 @@ pub trait BasicEnclaveUseCase: Sized {
     where
         C: ConfigGetter,
     {
-        let input = unsafe { std::slice::from_raw_parts_mut(input_buf, input_len) };
-        let res = unsafe {
-            match Self::start_use_case(input, enclave_context, ecall_max_size) {
-                Ok(out) => out,
-                Err(e) => {
-                    error!("Error in enclave (ecall_entry_point): {:?}", e);
-                    return frame_types::EnclaveStatus::error();
-                }
-            }
-        };
-
-        let res_len = res.len();
-        *output_len = res_len;
-
-        if res_len > ecall_max_size {
-            error!(
-                "Result buffer length is over ecall_max_size: ecall_max_size={}, res_len={}",
-                ecall_max_size, res_len
-            );
-            return frame_types::EnclaveStatus::error();
-        }
-        unsafe {
-            ptr::copy_nonoverlapping(res.as_ptr(), output_buf, res_len);
-        }
-
-        frame_types::EnclaveStatus::success()
-    }
-
-    fn start_use_case<C>(
-        input_payload: &[u8],
-        enclave_context: &C,
-        ecall_max_size: usize,
-    ) -> anyhow::Result<Vec<u8>>
-    where
-        C: ConfigGetter,
-    {
-        let res = {
-            let ecall_input = bincode::DefaultOptions::new()
-                .with_limit(ecall_max_size as u64)
-                .deserialize(&input_payload[..])
-                .map_err(|e| anyhow!("{:?}", e))?;
-
-            Self::run(ecall_input, enclave_context)?
-        };
-
-        bincode::serialize(&res).map_err(Into::into)
+        mk_input_ecall_entry_point(input_buf, input_len, ecall_max_size)
+            .and_then(|enclave_input| {
+                Self::run(enclave_input, enclave_context).and_then(|enclave_output| {
+                    mk_output_ecall_entry_point(
+                        enclave_output,
+                        output_buf,
+                        ecall_max_size,
+                        output_len,
+                    )
+                })
+            })
+            .unwrap_or_else(|e| {
+                error!("Error in enclave (ecall_entry_point): {:?}", e);
+                frame_types::EnclaveStatus::error()
+            })
     }
 }
