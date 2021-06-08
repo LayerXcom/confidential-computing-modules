e7de8b694903f65bd3a0245f60f2842d72b1b0b3
Sho Nakatani <lay.sakura@gmail.com>
2021-06-08 07:18:19 +0000

    refactor: StateRuntimeEnclaveUseCase to-be definition

--- frame/enclave/src/enclave_use_case/state_runtime_enclave_use_case.rs
+++ frame/enclave/src/enclave_use_case/state_runtime_enclave_use_case.rs
@@ -1,19 +1,48 @@
 use frame_common::{state_types::StateType, EnclaveInput, EnclaveOutput};
-use frame_runtime::{ContextOps, RuntimeExecutor};
+use frame_runtime::ContextOps;
+use log::error;
 use serde::{de::DeserializeOwned, Serialize};
 
-pub trait StateRuntimeEnclaveUseCase: Sized {
+use super::{mk_input_ecall_entry_point, mk_output_ecall_entry_point};
+
+pub trait StateRuntimeEnclaveUseCase<'c, C>: Sized
+where
+    C: ContextOps<S = StateType> + Clone,
+{
     type EI: EnclaveInput + DeserializeOwned;
     type EO: EnclaveOutput + Serialize;
 
-    fn new<C>(_enclave_input: Self::EI, _enclave_context: &C) -> anyhow::Result<Self>
-    where
-        C: ContextOps<S = StateType> + Clone;
+    fn new(enclave_input: Self::EI, enclave_context: &'c C) -> anyhow::Result<Self>;
 
     /// Evaluate policies like authentication and idempotency
     fn eval_policy(&self) -> anyhow::Result<()>;
 
-    fn run<C>(self, _enclave_context: &C, _max_mem_size: usize) -> anyhow::Result<Self::EO>
-    where
-        C: ContextOps<S = StateType> + Clone;
+    fn run(self) -> anyhow::Result<Self::EO>;
+
+    fn ecall_entry_point(
+        input_buf: *mut u8,
+        input_len: usize,
+        output_buf: *mut u8,
+        ecall_max_size: usize,
+        output_len: &mut usize,
+        enclave_context: &'c C,
+    ) -> frame_types::EnclaveStatus {
+        mk_input_ecall_entry_point(input_buf, input_len, ecall_max_size)
+            .and_then(|enclave_input| {
+                Self::new(enclave_input, enclave_context).and_then(|slf| {
+                    slf.run().and_then(|enclave_output| {
+                        mk_output_ecall_entry_point(
+                            enclave_output,
+                            output_buf,
+                            ecall_max_size,
+                            output_len,
+                        )
+                    })
+                })
+            })
+            .unwrap_or_else(|e| {
+                error!("Error in enclave (ecall_entry_point): {:?}", e);
+                frame_types::EnclaveStatus::error()
+            })
+    }
 }
