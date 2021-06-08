ead5a75708666c7b8a17901aa90277494efd9df1
Sho Nakatani <lay.sakura@gmail.com>
2021-06-08 07:30:41 +0000

    refactor: JoinGroupWithTreeKem compiles

--- modules/anonify-enclave/src/join_group/treekem.rs
+++ modules/anonify-enclave/src/join_group/treekem.rs
@@ -7,39 +7,39 @@ use frame_mra_tls::key_vault::request::BackupPathSecretRequestBody;
 use frame_runtime::traits::*;
 
 /// Joining the group with treekem-based handshake
-#[derive(Debug, Clone, Default)]
-pub struct JoinGroupWithTreeKem;
+#[derive(Debug, Clone)]
+pub struct JoinGroupWithTreeKem<'c, C> {
+    enclave_context: &'c C,
+}
 
-impl StateRuntimeEnclaveUseCase for JoinGroupWithTreeKem {
+impl<'c, C> StateRuntimeEnclaveUseCase<'c, C> for JoinGroupWithTreeKem<'c, C>
+where
+    C: ContextOps<S = StateType> + Clone,
+{
     type EI = input::Empty;
     type EO = output::ReturnJoinGroup;
 
-    fn new<C>(_enclave_input: Self::EI, _enclave_context: &C) -> anyhow::Result<Self>
-    where
-        C: ContextOps<S = StateType> + Clone,
-    {
-        Ok(Self::default())
+    fn new(_enclave_input: Self::EI, enclave_context: &'c C) -> anyhow::Result<Self> {
+        Ok(Self { enclave_context })
     }
 
     fn eval_policy(&self) -> anyhow::Result<()> {
         Ok(())
     }
 
-    fn run<C>(self, enclave_context: &C, _max_mem_size: usize) -> Result<Self::EO>
-    where
-        C: ContextOps<S = StateType> + Clone,
-    {
-        let attested_report = enclave_context.quote()?.remote_attestation(
-            enclave_context.ias_url(),
-            enclave_context.sub_key(),
-            enclave_context.ias_root_cert().to_vec(),
+    fn run(self) -> Result<Self::EO> {
+        let attested_report = self.enclave_context.quote()?.remote_attestation(
+            self.enclave_context.ias_url(),
+            self.enclave_context.sub_key(),
+            self.enclave_context.ias_root_cert().to_vec(),
         )?;
 
-        let (handshake, path_secret) = (&*enclave_context.read_group_key()).create_handshake()?;
+        let (handshake, path_secret) =
+            (&*self.enclave_context.read_group_key()).create_handshake()?;
         let epoch = handshake.prior_epoch();
         let id = handshake.hash();
         let export_path_secret = path_secret.clone().try_into_exporting(epoch, id.as_ref())?;
-        enclave_context
+        self.enclave_context
             .store_path_secrets()
             .save_to_local_filesystem(&export_path_secret)?;
         let export_handshake = handshake.clone().into_export();
@@ -52,14 +52,15 @@ impl StateRuntimeEnclaveUseCase for JoinGroupWithTreeKem {
                 handshake.roster_idx(),
                 id.as_ref().to_vec(),
             );
-            enclave_context.backup_path_secret(backup_path_secret)?;
+            self.enclave_context
+                .backup_path_secret(backup_path_secret)?;
         }
 
         Ok(output::ReturnJoinGroup::new(
             attested_report.report().to_vec(),
             attested_report.report_sig().to_vec(),
             Some(export_handshake.encode()),
-            enclave_context.mrenclave_ver(),
+            self.enclave_context.mrenclave_ver(),
             export_handshake.roster_idx(),
         ))
     }
