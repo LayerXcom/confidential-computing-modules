1d6d6ef6bcc49c84718a035451d0ac3b2e6ddd9e
Sho Nakatani <lay.sakura@gmail.com>
2021-06-08 11:58:33 +0000

    refactor: rm unnecessary macro code

--- frame/enclave/src/register_enclave_use_case.rs
+++ frame/enclave/src/register_enclave_use_case.rs
@@ -8,36 +8,6 @@ macro_rules! register_enclave_use_case {
             ($cmd: path, $use_case: ty),
         )*
     ) => {
-        #[cfg(feature = "runtime_enabled")]
-        fn ecall_handler(cmd: u32, input: &mut [u8], ecall_max_size: usize) -> anyhow::Result<Vec<u8>> {
-            match cmd {
-                $(
-                    $(#[$feature])*
-                    $cmd => inner_ecall_handler::<$use_case>(input, ecall_max_size),  // TODO ここでマクロで定義した関数呼び出しではなく、EnclaveUseCaseのデフォルト関数呼び出しにする（マクロを小さくする）
-                )*
-                _ => anyhow::bail!("Not registered the ecall command"),
-            }
-        }
-
-        #[cfg(feature = "runtime_enabled")]
-        fn inner_ecall_handler<EE>(input_payload: &[u8], ecall_max_size: usize) -> anyhow::Result<Vec<u8>>
-        where
-            EE: StateRuntimeEnclaveUseCase,
-        {
-            let res = {
-                let enclave_input = bincode::DefaultOptions::new()
-                    .with_limit(ecall_max_size as u64)
-                    .deserialize(&input_payload[..])
-                    .map_err(|e| anyhow!("{:?}", e))?;
-
-                let slf = EE::new::<$ctx_ops>(enclave_input, $ctx)?;
-                EE::eval_policy(&slf)?;
-                EE::run::<$runtime_exec, $ctx_ops>(slf, $ctx, $max_mem)?
-            };
-
-            bincode::serialize(&res).map_err(Into::into)
-        }
-
         #[no_mangle]
         pub extern "C" fn ecall_entry_point(
             cmd: u32,
