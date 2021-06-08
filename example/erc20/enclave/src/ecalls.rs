ec198bd9f396f01853f4b8a011083b9390a8641b
Sho Nakatani <lay.sakura@gmail.com>
2021-06-08 11:51:16 +0000

    fix: type parameters to UseCases

--- example/erc20/enclave/src/ecalls.rs
+++ example/erc20/enclave/src/ecalls.rs
@@ -17,55 +17,55 @@ register_enclave_use_case!(
     #[cfg(feature = "enclave_key")]
     (
         SEND_COMMAND_ENCLAVE_KEY_CMD,
-        CommandByEnclaveKeySender<NoAuth>
+        CommandByEnclaveKeySender<AnonifyEnclaveContext, Runtime<AnonifyEnclaveContext>,NoAuth>
     ),
     #[cfg(feature = "treekem")]
     (
         SEND_COMMAND_TREEKEM_CMD,
-        CommandByTreeKemSender<NoAuth>
+        CommandByTreeKemSender<AnonifyEnclaveContext, Runtime<AnonifyEnclaveContext>,NoAuth>
     ),
     #[cfg(feature = "enclave_key")]
     (
         FETCH_CIPHERTEXT_ENCLAVE_KEY_CMD,
-        CommandByEnclaveKeyReceiver<NoAuth>
+        CommandByEnclaveKeyReceiver<AnonifyEnclaveContext, Runtime<AnonifyEnclaveContext>,NoAuth>
     ),
     // Fetch a ciphertext in event logs from blockchain nodes into enclave's memory database.
     #[cfg(feature = "treekem")]
     (
         FETCH_CIPHERTEXT_TREEKEM_CMD,
-        CommandByTreeKemReceiver<NoAuth>
+        CommandByTreeKemReceiver<AnonifyEnclaveContext, Runtime<AnonifyEnclaveContext>,  NoAuth>
     ),
     #[cfg(feature = "treekem")]
-    (SEND_HANDSHAKE_TREEKEM_CMD, HandshakeSender),
+    (SEND_HANDSHAKE_TREEKEM_CMD, HandshakeSender<AnonifyEnclaveContext>),
     // Fetch handshake received from blockchain nodes into enclave.
     #[cfg(feature = "treekem")]
-    (FETCH_HANDSHAKE_TREEKEM_CMD, HandshakeReceiver),
+    (FETCH_HANDSHAKE_TREEKEM_CMD, HandshakeReceiver<AnonifyEnclaveContext>),
     // Get current state of the user represented the given public key from enclave memory database.
     #[cfg(feature = "enclave_key")]
-    (JOIN_GROUP_ENCLAVE_KEY_CMD, JoinGroupWithEnclaveKey),
+    (JOIN_GROUP_ENCLAVE_KEY_CMD, JoinGroupWithEnclaveKey<AnonifyEnclaveContext>),
     #[cfg(feature = "treekem")]
-    (JOIN_GROUP_TREEKEM_CMD, JoinGroupWithTreeKem),
-    (GET_STATE_CMD, GetState<NoAuth>),
+    (JOIN_GROUP_TREEKEM_CMD, JoinGroupWithTreeKem<AnonifyEnclaveContext>),
+    (GET_STATE_CMD, GetState<AnonifyEnclaveContext,Runtime<AnonifyEnclaveContext>,NoAuth>),
     (
         REGISTER_NOTIFICATION_CMD,
-        RegisterNotification<NoAuth>
+        RegisterNotification<AnonifyEnclaveContext,NoAuth>
     ),
-    (GET_ENCLAVE_ENCRYPTION_KEY_CMD, EncryptionKeyGetter),
-    (SEND_REGISTER_REPORT_CMD, ReportRegistration),
+    (GET_ENCLAVE_ENCRYPTION_KEY_CMD, EncryptionKeyGetter<AnonifyEnclaveContext>),
+    (SEND_REGISTER_REPORT_CMD, ReportRegistration<AnonifyEnclaveContext>),
     #[cfg(feature = "treekem")]
     #[cfg(feature = "backup-enable")]
-    (BACKUP_PATH_SECRETS_CMD, PathSecretsBackupper),
+    (BACKUP_PATH_SECRETS_CMD, PathSecretsBackupper<AnonifyEnclaveContext>),
     #[cfg(feature = "treekem")]
     #[cfg(feature = "backup-enable")]
-    (RECOVER_PATH_SECRETS_CMD, PathSecretsRecoverer),
+    (RECOVER_PATH_SECRETS_CMD, PathSecretsRecoverer<AnonifyEnclaveContext>),
     (
         GET_USER_COUNTER_CMD,
-        GetUserCounter<NoAuth>
+        GetUserCounter<AnonifyEnclaveContext,NoAuth>
     ),
     #[cfg(feature = "enclave_key")]
     #[cfg(feature = "backup-enable")]
-    (BACKUP_ENCLAVE_KEY_CMD, EnclaveKeyBackupper),
+    (BACKUP_ENCLAVE_KEY_CMD, EnclaveKeyBackupper<AnonifyEnclaveContext>),
     #[cfg(feature = "enclave_key")]
     #[cfg(feature = "backup-enable")]
-    (RECOVER_ENCLAVE_KEY_CMD, EnclaveKeyRecoverer),
+    (RECOVER_ENCLAVE_KEY_CMD, EnclaveKeyRecoverer<AnonifyEnclaveContext>),
 );
