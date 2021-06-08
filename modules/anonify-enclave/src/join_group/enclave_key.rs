6117eaa0f1ff1860133059d69db0fd4a1280320c
Sho Nakatani <lay.sakura@gmail.com>
2021-06-08 07:33:19 +0000

    refactor: JoinGroupWithEnclaveKey compiles

--- modules/anonify-enclave/src/join_group/enclave_key.rs
+++ modules/anonify-enclave/src/join_group/enclave_key.rs
@@ -5,40 +5,39 @@ use frame_enclave::StateRuntimeEnclaveUseCase;
 use frame_runtime::traits::*;
 
 /// A add handshake Sender
-#[derive(Debug, Clone, Default)]
-pub struct JoinGroupWithEnclaveKey;
+#[derive(Debug, Clone)]
+pub struct JoinGroupWithEnclaveKey<'c, C> {
+    enclave_context: &'c C,
+}
 
-impl StateRuntimeEnclaveUseCase for JoinGroupWithEnclaveKey {
+impl<'c, C> StateRuntimeEnclaveUseCase<'c, C> for JoinGroupWithEnclaveKey<'c, C>
+where
+    C: ContextOps<S = StateType> + Clone,
+{
     type EI = input::Empty;
     type EO = output::ReturnJoinGroup;
 
-    fn new<C>(_enclave_input: Self::EI, _enclave_context: &C) -> anyhow::Result<Self>
-    where
-        C: ContextOps<S = StateType> + Clone,
-    {
-        Ok(Self::default())
+    fn new(_enclave_input: Self::EI, enclave_context: &'c C) -> anyhow::Result<Self> {
+        Ok(Self { enclave_context })
     }
 
     fn eval_policy(&self) -> anyhow::Result<()> {
         Ok(())
     }
 
-    fn run<C>(self, enclave_context: &C, _max_mem_size: usize) -> Result<Self::EO>
-    where
-        C: ContextOps<S = StateType> + Clone,
-    {
-        let attested_report = enclave_context.quote()?.remote_attestation(
-            enclave_context.ias_url(),
-            enclave_context.sub_key(),
-            enclave_context.ias_root_cert().to_vec(),
+    fn run(self) -> Result<Self::EO> {
+        let attested_report = self.enclave_context.quote()?.remote_attestation(
+            self.enclave_context.ias_url(),
+            self.enclave_context.sub_key(),
+            self.enclave_context.ias_root_cert().to_vec(),
         )?;
 
         Ok(output::ReturnJoinGroup::new(
             attested_report.report().to_vec(),
             attested_report.report_sig().to_vec(),
             None,
-            enclave_context.mrenclave_ver(),
-            enclave_context.my_roster_idx() as u32,
+            self.enclave_context.mrenclave_ver(),
+            self.enclave_context.my_roster_idx() as u32,
         ))
     }
 }
